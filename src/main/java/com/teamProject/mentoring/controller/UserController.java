d6ebee0779af5fcd4fbec4fb516cf7a97284f09e
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-08-11 15:20:58 +0900

    add sign_up_in page

--- src/main/java/com/teamProject/mentoring/controller/UserController.java
+++ src/main/java/com/teamProject/mentoring/controller/UserController.java
@@ -15,33 +15,35 @@ import java.util.Optional;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("user/register")
+    @GetMapping("user/sign_up_in")
     public String registerHome(){
-        return "registerHome";
+        return "sign_up_in";
     }
 
-    @PostMapping("user/register")
+    @PostMapping("user/sign_up")
     public String save(@ModelAttribute UserDto userDto){
 
         System.out.println(userDto.toString());
 
 
-        if(userService.save(userDto)) return "loginHome";
+        // 회원가입 성공 시 로그인 페이지.
+        if(userService.save(userDto)) return "sign_up_in";
+        // 회원가입 실패 (이메일 중복) 시 알림창 띄우고 다시 회원가입 페이지
         else return "registerHome";
     }
-    @GetMapping("user/login")
-    public String login(){
-        return "loginHome";
-    }
+//    @GetMapping("user/login")
+//    public String login(){
+//        return "loginHome";
+//    }
 
-    @PostMapping("user/login")
+    @PostMapping("user/sign_in")
     public String login(@ModelAttribute UserDto userDto, HttpSession session){
         System.out.println("로그인 dto = "+userDto.toString());
         UserDto loginResult = userService.login(userDto);
         if(loginResult != null){
             // 로그인 성공
             // 앞으로 이 세션 정보를 이용.
-            session.setAttribute("loginEmail", loginResult.getEmail());
+            session.setAttribute("userName", loginResult.getName());
             System.out.println("성공!");
             return "mainPage";
         }
