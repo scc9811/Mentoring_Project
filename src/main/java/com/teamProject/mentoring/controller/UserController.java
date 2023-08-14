0bb8bffa81bac0a49e25954ccaa34addeccf943f
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-08-14 16:34:50 +0900

    로그인/회원가입 alert 추가, 프로그램들 객체생성, db연결

--- src/main/java/com/teamProject/mentoring/controller/UserController.java
+++ src/main/java/com/teamProject/mentoring/controller/UserController.java
@@ -2,13 +2,17 @@ package com.teamProject.mentoring.controller;
 
 
 import com.teamProject.mentoring.dto.UserDto;
+import com.teamProject.mentoring.entity.Message;
 import com.teamProject.mentoring.service.UserService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.servlet.ModelAndView;
 import java.util.Optional;
+
+
 
 @Controller
 @RequiredArgsConstructor
@@ -21,23 +25,29 @@ public class UserController {
     }
 
     @PostMapping("user/sign_up")
-    public String save(@ModelAttribute UserDto userDto){
+    public ModelAndView save(@ModelAttribute UserDto userDto, ModelAndView modelAndView){
 
         System.out.println(userDto.toString());
 
 
         // 회원가입 성공 시 로그인 페이지.
-        if(userService.save(userDto)) return "sign_up_in";
+        if(userService.save(userDto)) {
+            System.out.println("성공");
+            modelAndView.addObject("data", new Message("회원가입 성공.", "/user/sign_up_in"));
+        }
         // 회원가입 실패 (이메일 중복) 시 알림창 띄우고 다시 회원가입 페이지
-        else return "registerHome";
+        else {
+            System.out.println("실패");
+            modelAndView.addObject("data", new Message("이미 가입된 이메일입니다. 다시 회원가입 해주세요.", "/user/sign_up_in"));
+        }
+        modelAndView.setViewName("Message");
+
+
+        return modelAndView;
     }
-//    @GetMapping("user/login")
-//    public String login(){
-//        return "loginHome";
-//    }
 
     @PostMapping("user/sign_in")
-    public String login(@ModelAttribute UserDto userDto, HttpSession session){
+    public ModelAndView login(@ModelAttribute UserDto userDto, HttpSession session, ModelAndView modelAndView){
         System.out.println("로그인 dto = "+userDto.toString());
         UserDto loginResult = userService.login(userDto);
         if(loginResult != null){
@@ -45,13 +55,23 @@ public class UserController {
             // 앞으로 이 세션 정보를 이용.
             session.setAttribute("userName", loginResult.getName());
             System.out.println("성공!");
-            return "mainPage";
+            modelAndView.addObject("data", new Message("", "/mainPage"));
+//            return "mainPage";
         }
         else{
             // 로그인 실패
             System.out.println("실패!");
-            return "loginHome";
+            modelAndView.addObject("data", new Message("실패", "/user/sign_up_in"));
+//            return "loginHome";
         }
+        modelAndView.setViewName("Message");
+        return modelAndView;
+    }
+
+
+    @GetMapping("mainPage")
+    public String mainPage(){
+        return "mainPage";
     }
 
 
